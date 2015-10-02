b2c0982c47e11b394afb4c79dcc5e2bf94007e13
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-10-02 14:01:14 +0300

    update to 1.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ var pathTo = {
             BowerJSVendor: 'src/js/vendor/',
             JSCustom: ['custom/**/*.js', 'main.js', '!custom/toHead/**/*.js'],
             CSSVendor: 'src/scss/vendor/',
-            Txt: ['src/humans.txt', 'src/robots.txt'],
+            Txt: ['src/humans.txt', 'src/robots.txt', '.htaccess','CHANGELOG.md','README.md'],
             Fonts: 'src/scss/fonts/**/*',
             Svg: ['src/svg/**/*.*', '!src/svg/sprite/*.*'],
             SvgSprite: 'src/svg/sprite/**/*.svg',
---
+++ src/CHANGELOG.md
@@ -0,0 +1,10 @@
+### 1.0.2 (October 2, 2015)
+
+* add CHANGELOG, README
+* add ./.htaccess template
+* add ./editorconfig template
+* add Google Analytics template in src/jade/includes/_footer.jade
+* add Yandex.Metrika template in src/jade/includes/_footer.jade
+* update ./humans.txt 
+* fix sass-mq vars in src/scss/includes/_variables.scss
+* move version to 1.0.2
